ec9d9067f769141f2c0d2ace6300eeaf53189e87
onrioni <onlyoni20@naver.com>
2022-12-23 15:08:26 +0900

    basic set

---
+++ src/server.js
@@ -0,0 +1,25 @@
+
+import express from "express";
+import morgan from "morgan";
+
+const PORT = 4000;
+
+const app = express();
+const logger = morgan("dev");
+
+const home = (req, res) => {
+    console.log("I will respond.");
+    return res.send("I love middlewares");
+};
+const login = (req, res) => {
+    return res.send("login");
+}
+app.use(logger);
+app.get("/", home);
+app.get("/login", login);
+
+const handleListening = () => 
+console.log(`Server listening on port http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
+

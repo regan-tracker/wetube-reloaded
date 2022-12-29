213aea3f59f2f4746021ac89de2b4cc923f8754c
onrioni <onlyoni20@naver.com>
2022-12-29 22:14:43 +0900

    added home pug file

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,7 @@
-export const trending = (req, res) => res.send("Home Page Videos");
+export const trending = (req, res) => 
+    res.render("home");
 
 export const see = (req, res) => {
-    console.log(req.params);
     return res.send("Watch");
 }
 export const edit = (req, res) => {

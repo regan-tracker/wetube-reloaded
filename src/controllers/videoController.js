e4ce3f4964ce7e8de8cb467733e3434dfccb35e1
onrioni <onlyoni20@naver.com>
2022-12-30 00:00:33 +0900

    Use extends & block

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -3,9 +3,7 @@ export const trending = (req, res) =>
 
 export const see = (req, res) => res.render("watch");
 
-export const edit = (req, res) => {
-    return res.send("Edit");
-}
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => {

912c68e46ad7fe7527140c16a70103af1b6e571d
onrioni <onlyoni20@naver.com>
2022-12-29 22:51:47 +0900

    pug partial with include

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,9 +1,8 @@
 export const trending = (req, res) => 
     res.render("home");
 
-export const see = (req, res) => {
-    return res.send("Watch");
-}
+export const see = (req, res) => res.render("watch");
+
 export const edit = (req, res) => {
     return res.send("Edit");
 }

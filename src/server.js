ac6327eaebe80fe5debee08804894ce9f1ffab23
onrioni <onlyoni20@naver.com>
2023-01-16 11:04:38 +0900

    post video with mongodb

--- src/server.js
+++ src/server.js
@@ -1,12 +1,10 @@
-import "./db";
-import "./models/Video";
+
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 
-const PORT = 4000;
 
 
 const app = express();
@@ -20,9 +18,5 @@ app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
-
-const handleListening = () => 
-console.log(`Server listening on port http://localhost:${PORT}`);
-
-app.listen(PORT, handleListening);
+export default app;
 
